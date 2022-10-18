bf1ea8cfbf078011280bfd6b6c93e70d7192b3ef
Naveen Chowdhury <naveenchowdhury2000@gmail.com>
2022-10-17 20:41:18 -0400

    initial skeleton java code added

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # nav-password-generator
 Creating the first password generator 
+
+## My Task 
+Modifying started code in JavaScript file to create an application that ensures a random password following a set criteria laid out by a company/website. The JavaScipt code will work alongside HTML & CSS files to make a dynamic site where prospective users can make their accounts using a highly secure passwords. The goal is the make a seamless process where the actions are laid out in concise and accessible manner to create sensitive data.
+
+## Checklist of Acceptance Criteria 
+
+```
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+```
+
+## Screenshot of Completed Project 
+
--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Personal Code that was added//
+var passwordLength = window.prompt("Choose the length of your password (in the range of 8-128 characters): ");
+var useUpper = window.confirm("Would you like to implement Upper-case characters to make your password safer? (yes/no)");
+console.log(useUpper);
+var useLower = window.confirm("Would you like to implement Lower-case characters to make your password dynamic? (yes/no)");
+var useNumbers = window.confirm("Would you like to use numbers in your randomly generated password?");
+var useCharacters = window.confirm("Would you like to include 'Special Characters' in your password?");
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +20,68 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Defining Conditions of the Generator//
+
+    //1. Password Length condition
+if (+passwordLength > 128)
+{
+  alert("Your password is too long! Please make sure it is between the range of 8-128");
+}   else if (+passwordLength < 8) 
+{
+  alert("Your passwword is too short!Please make sure it is between the range of 8-128");
+}
+
+    //2. creation of stored prompt variables [arrays] 
+var upperCaseVariables = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var lowerCaseVariables = ["abcdefghijklmnopqrstuvwxyz"];
+var numberVariables = [1234567890];
+var specialCharacters = ["!@#$%^&*()_+"];
+
+    //3. Conditions for ^ initial variables [prompt]
+if (useUpper == undefined && useLower == undefined && useNumbers == undefined && useCharacters == undefined)
+{
+  window.alert("You have selected an invalid combination!");
+}
+    //4. Declaration of all characters. 
+var characteristics = '';
+
+if (useUpper === true){
+  var characteristics = characteristics + upperCaseVariables;
+}
+if (useLower === true){
+  var characteristics = characteristics + lowerCaseVariables;
+}
+if (useNumbers === true){
+  var characteristics = characteristics + numberVariables;
+}
+if (useCharacters === true){
+  var characteristics = characteristics + specialCharacters;
+}
+
+function generatePassword(length) {
+//reusing prompt from the beginning//
+  var passwordLength = window.prompt("Choose the length of your password (in the range of 8-128 characters): ");
+  var useUpper = window.confirm("Would you like to implement Upper-case characters to make your password safer? (yes/no)");
+console.log(useUpper);
+  var useLower = window.confirm("Would you like to implement Lower-case characters to make your password dynamic? (yes/no)");
+  var useNumbers = window.confirm("Would you like to use numbers in your randomly generated password?");
+  var useCharacters = window.confirm("Would you like to include 'Special Characters' in your password?");
+//adding results 
+  let result = '';
+  var characteristicsLength = characteristics.length;
+  for (let i = 0; i < passwordLength; i++) {
+    result += characteristics.charAt(Math.floor(Math.random() * characteristicsLength));
+  }
+
+  return result;
+
+}
+
+//Returning the desired outcome/password. 
+var password = generatePassword(+passwordLength);
+console.log(password);
+
+
+
+
